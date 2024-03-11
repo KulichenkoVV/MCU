42cff7f2fb8f3fee6c308970f079d75f27b9afc8
MYKHAILO <162332432+Whiterset@users.noreply.github.com>
2024-03-11 14:21:29 +0200

    updt

---
+++ PROG1/PROG1/Debug/components1/led.d
@@ -0,0 +1,2 @@
+components1/led.o: ../components1/led.c ../components1/led.h
+../components1/led.h:
---
+++ PROG1/PROG1/components1/led.c
@@ -0,0 +1,11 @@
+/*
+ * led.c
+ *
+ *  Created on: Mar 11, 2024
+ *      Author: AMDD
+ */
+
+#include "led.h"
+
+void led_init(void);
+void led_set_state(led_s_t led);
